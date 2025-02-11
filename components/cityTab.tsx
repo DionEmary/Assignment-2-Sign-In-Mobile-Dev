ef15c6b6d43a62695e252eb5a43b187e8db83780
benferaer <benferaer@gmail.com>
2025-02-11 14:51:58 -0700

    Removed scrollview and updated Edmonton trivia

--- components/cityTab.tsx
+++ components/cityTab.tsx
@@ -15,7 +15,7 @@ const CityTabs = () => {
         Edmonton: {
             image: require('../assets/edmonton.jpg'),
             link: 'https://www.edmonton.ca/',
-            info: 'Edmonton, the capital city of Alberta, Canada, is often called the "Gateway to the North" due to its strategic location as a hub for oil and gas industries and its proximity to the northern wilderness. The city\'s rich cultural scene includes the Edmonton International Fringe Festival, one of the largest fringe theater festivals in the world, attracting artists and visitors from all over the globe.',
+            info: 'Edmonton, the capital city of Alberta, Canada, is often called the "Gateway to the North" due to its strategic location as a hub for oil and gas industries and its proximity to the northern wilderness. Check out the Edmonton International Fringe Festival which attracts artists and visitors from all over the globe.',
         }
     }
 
@@ -29,11 +29,9 @@ const CityTabs = () => {
                     <Text style={[styles.tabText, city === 'Edmonton' && styles.activeText]}>Edmonton</Text>
                 </TouchableOpacity>
             </View>
-            <ScrollView>
               <Image source={cityInfo[city].image} style={styles.cityImage} />
               <CityInfo info={cityInfo[city].info} />
               <CityLink link={cityInfo[city].link} />
-            </ScrollView>
         </View>
     );
 }
